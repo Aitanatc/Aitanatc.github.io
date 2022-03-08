1073ea0c71734699861678a0745699ccd95d93dd
Aitana <yuqua29@gmail.com>
2022-03-08 01:00:57 -0500

    map base

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -4,3 +4,5 @@ const mainnav = document.querySelector('.navigation')
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+<script async defer src="https://goo.gl/maps/CL277vnDA4xfV5u87"></script>
