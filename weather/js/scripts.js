3618452ee8ac61bed2ad10868249b88ca3d00d95
Aitana <yuqua29@gmail.com>
2022-03-08 18:57:10 -0500

    map in every page

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -5,4 +5,3 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-<script async defer src="https://goo.gl/maps/CL277vnDA4xfV5u87"></script>
