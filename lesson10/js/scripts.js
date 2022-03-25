f0533d5abf5280fbb2d0c4d991e7f31e17ce3640
Aitana <yuqua29@gmail.com>
2022-03-25 00:06:53 -0500

    api

---
+++ lesson10/js/scripts.js
@@ -0,0 +1,44 @@
+const hambutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive');}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive');};
+/////////// wind child
+let speed = 2;
+let temp = 18;
+
+let windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+
+console.log(windChill);
+
+windChill = Math.floor(windChill);
+
+windChill = (windChill > temp) ? temp : windChill;
+
+console.log(windChill);
+
+document.querySelector("#wind-chill").textContent = windChill + '°F';
+
+////////// api json weather
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&appid=0205b06aab5afe13a29d9528615f45fe&units=imperial";
+fetch(apiURL)
+    .then((Response) => Response.json())
+    .then((jsObject) =>{
+        console.log(jsObject);
+        const summary = jsObject;
+        document.querySelector('#currently').innerHTML = summary.weather[0].main;
+        document.querySelector('#temperature').textContent = summary.main.temp;
+        document.querySelector('#humidity').innerHTML = summary.main.humidity;
+        document.querySelector('#windSpeed').innerText = summary.wind.speed;
+
+    });
+//// five day forecast
+const forecastArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const forecastURL = "http://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&appid=0205b06aab5afe13a29d9528615f45fe&units=imperial";
+// fetch(forecastURL)
+//     .then((response) => response.json())
+//     .then((jsObject) => {
+//         console.table(jsObject);
+//     }
+
